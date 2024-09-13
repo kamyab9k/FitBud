b5890834e691d9d419567cd8a9e290dd83ec248a
kamyab <mohamad.khsvi@gmail.com>
2024-09-13 22:13:41 +0330

    RegisterScreen implemented

--- app/src/main/java/com/example/fitbud/registration/ui/RegisterScreen.kt
+++ app/src/main/java/com/example/fitbud/registration/ui/RegisterScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -22,6 +24,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAbsoluteAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -52,12 +55,13 @@ fun RegisterScreen() {
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.SpaceBetween // Space between elements
+        verticalArrangement = Arrangement.Center
     ) {
         Column(
             modifier = Modifier
                 .weight(1f) // Takes up the remaining space
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .padding(bottom = 64.dp),
             verticalArrangement = Arrangement.Center
         ) {
             Text(
@@ -68,7 +72,7 @@ fun RegisterScreen() {
 
             Spacer(modifier = Modifier.height(40.dp))
 
-            // Username input
+
             OutlinedTextField(
                 value = username,
                 onValueChange = { username = it },
@@ -79,7 +83,6 @@ fun RegisterScreen() {
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Email input
             OutlinedTextField(
                 value = email,
                 onValueChange = { email = it },
@@ -90,7 +93,7 @@ fun RegisterScreen() {
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Password input
+
             OutlinedTextField(
                 value = password,
                 onValueChange = { password = it },
@@ -105,7 +108,7 @@ fun RegisterScreen() {
                 pushStringAnnotation(tag = "URL", annotation = "https://github.com/kamyab9k")
                 withStyle(
                     style = SpanStyle(
-                        color = Color.Blue,  // Set the color to blue
+                        color = Color.Blue,
                         textDecoration = TextDecoration.Underline
                     )
                 ) {
@@ -122,7 +125,6 @@ fun RegisterScreen() {
                     fontWeight = FontWeight.Normal,
                 ),
                 modifier = Modifier.clickable {
-                    // Handle the click event for "Terms"
                     annotatedString.getStringAnnotations("URL", 0, annotatedString.length)
                         .firstOrNull()?.let { annotation ->
                             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(annotation.item))
@@ -132,16 +134,18 @@ fun RegisterScreen() {
             )
             Spacer(modifier = Modifier.height(24.dp))
 
-            // Register Button
+
             Button(
                 onClick = {
                     // Handle register action here
                 },
                 modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
                     .fillMaxWidth()
                     .height(48.dp),
+
                 colors = androidx.compose.material3.ButtonDefaults.buttonColors(
-                    containerColor = Color.Black,  // Button background color
+                    containerColor = Color.Black,
                     contentColor = Color.White     // Button text color
                 )
             ) {
